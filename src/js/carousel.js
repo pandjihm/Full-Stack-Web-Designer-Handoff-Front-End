45c672c59997ea7d35683dd2acbf67bf99e51e67
pandjihm <pandjihm@yahoo.com>
2021-02-01 23:04:10 +0700

    JS Carousel (2)

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,6 +1,6 @@
 import { addClass, removeClass } from "./utils-class.js";
 
-const carouselID = document.getElementById("carousel")
+const carouselId = document.getElementById("carousel")
 const carouselItems = document.getElementsByClassName("flex")[0]
 const carouselContainer = document.getElementsByClassName("container")[0]
 
@@ -12,7 +12,62 @@ function carouselCalculateOffset() {
 
 }
 
-if (carouselID) {
+function slide (wrapper, items) {
+    let posX1 = 0, 
+        posX2 = 0, 
+        posInitial, 
+        posFinal, 
+        threshold = 100, 
+        itemToShow = 4,
+        slides = items.getElementsByClassName("card"),
+        slidesLength = slides.length,
+        slideSize = items.getElementsByClassName("card")[0].offsetWidth,
+        index = 0,
+        allowShift = true;
+
+        wrapper.classList.add("loaded");
+
+        items.onmousedown = dragStart;
+
+        items.addEventListener("touchstart", dragStart);
+        items.addEventListener("touchend", dragEnd);
+        items.addEventListener("touchmove", dragAction);
+
+        items.addEventListener("transitionend", checkIndex);
+
+        function dragStart(event){
+            event = event || window.event
+            event.preventDefault()
+            posInitial = items.offsetLeft;
+
+            if(event.type == "touchStart") {
+                posX1 = event.touches[0].clientX
+            }
+            else {
+                posX1 = event.clientX;
+                document.onmouseup = dragEnd;
+                document.onmousemove = dragAction;
+            }
+        }
+        
+        function dragAction(event){
+            event = event || window.event
+
+            if(event.type == "touchmove") {
+                posX2 = posX1 - event.touches[0].clientX
+                posX1 = event.touches[0].clientX
+            }
+            else {
+                posX2 = posX1 - event.clientX
+                posX1 = event.clientX
+            }
+
+            items.style.Left = `${items.offsetLeft - posX2}px`
+        }
+}
+
+if (carouselId) {
+    slide(carouselId, carouselItems);
     window.addEventListener("load", carouselCalculateOffset);
     window.addEventListener("resize", carouselCalculateOffset);
 }
